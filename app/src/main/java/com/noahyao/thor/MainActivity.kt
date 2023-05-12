4c746bb1f96e540be5748c9a4e859dacd3467b43
Noah Yao <noahyao1024@gmail.com>
2023-05-12 14:15:27 +0800

    almost there.

--- app/src/main/java/com/noahyao/thor/MainActivity.kt
+++ app/src/main/java/com/noahyao/thor/MainActivity.kt
@@ -1,27 +1,65 @@
+
 package com.noahyao.thor
 
+import android.content.DialogInterface
 import android.os.Bundle
+import android.view.Window
+import android.webkit.JsResult
+import android.webkit.WebChromeClient
 import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import server.Server
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
 
-
-        var host = "http://localhost"
-        val port = "9000"
-        var uri = "fakehtml/reports.html"
+        var url = "http://localhost:9000/fakehtml/reports.html"
         requestPermissions(arrayOf("android.permission.WRITE_EXTERNAL_STORAGE"), 100)
-        Server.listenAtBackground(port)
+        Server.listenAtBackground("9000")
 
         // Load WebView
         val webView = findViewById<WebView>(R.id.webView)
         webView.settings.javaScriptEnabled = true
-        webView.loadUrl("$host:$port/$uri")
+
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onJsAlert(
+                view: WebView?,
+                url: String?,
+                message: String?,
+                result: JsResult?
+            ): Boolean {
+                return super.onJsAlert(view, url, message, result)
+            }
+
+            override fun onReceivedTitle(view: WebView?, title: String?) {
+                super.onReceivedTitle(view, "")
+            }
+        }
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+            }
+        }
+
+        webView.loadUrl(url)
+        webView.settings.setSupportZoom(false)
+        webView.settings.builtInZoomControls = false
+        webView.settings.displayZoomControls = false
+        webView.settings.domStorageEnabled = false
+        webView.settings.databaseEnabled = false
+        webView.settings.useWideViewPort = false
+        webView.settings.loadWithOverviewMode = false
+        webView.settings.setGeolocationEnabled(false)
+        webView.settings.userAgentString = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
     }
 
     override fun onRequestPermissionsResult(
@@ -30,3 +68,5 @@ class MainActivity : AppCompatActivity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 }
+
+
