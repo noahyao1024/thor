4854961a020870a130ea5f5ff3e09d244d87d2e1
Noah Yao <noahyao1024@gmail.com>
2023-05-12 13:19:11 +0800

    fix init db.

--- go/server/container/db.go
+++ go/server/container/db.go
@@ -29,6 +29,27 @@ func InitDB() {
 		if err != nil {
 			panic(fmt.Sprintf("%s - %s", strings.TrimRight(sqlLitePath, "/")+"/crucial.db", err.Error()))
 		}
+
+		localDBClient.Exec(`CREATE TABLE IF NOT EXISTS reports (
+				id INTEGER PRIMARY KEY AUTOINCREMENT,
+				name TEXT NOT NULL,
+				data TEXT NULL,
+				status TEXT NULL,
+				create_time DATETIME DEFAULT (datetime('now')),
+				modify_time DATETIME DEFAULT (datetime('now', 'localtime'))
+			  );
+			  CREATE TABLE IF NOT EXISTS cases (
+				id INTEGER PRIMARY KEY AUTOINCREMENT,
+				template_id INTEGER,
+				template_name TEXT NULL,
+				report_id INTEGER NULL,
+				status TEXT NULL,
+				data TEXT NULL,
+				apis TEXT NULL,
+				create_time DATETIME DEFAULT (datetime('now')),
+				modify_time DATETIME DEFAULT (datetime('now', 'localtime'))
+			  );
+			  `)
 	}
 }
 
